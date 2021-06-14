1e01075b55d93df931cc29be2f0b7d0ec2f716ea
baharozcelik <baharozc@hotmail.com>
2021-06-14 11:14:52 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import type {Node} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Slider from './components/Slider.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -15,7 +15,17 @@ const images = [
   'https://i.dr.com.tr/pimages/Content/Uploads/BannerFiles/dr/0521_d_u_x_851x373_haftanin_yayinevi_redhouse.jpg'
 ]
 
+const dataList = [{key:'1'},{key:'2'},{key:'3'},{key:'4'},{key:'5'}]
+
 export default class App extends React.Component {
+
+  _renderItem = ({item, index}) => {
+    return (
+      <View style={styles.item} >
+        <Text>{item.key}</Text>
+      </View>
+    )
+  }
 
   render() {
     return (
@@ -25,6 +35,11 @@ export default class App extends React.Component {
           <SearchBar/>
         </Screen>
         <Slider images={images}/>
+        <FlatList
+          data={dataList}
+          renderItem={this._renderItem}
+          keyExtractor={(item,index) => index.toString()}
+        ></FlatList>
       </View>
 
     )
@@ -33,6 +48,15 @@ export default class App extends React.Component {
 
 const style = StyleSheet.create({
   container: {
-    marginTop: 50
+    marginTop: 20
+  },
+})
+
+  const styles = StyleSheet.create({
+    item: {
+      backgroundColor: '#3232ff',
+     // alignItems: 'center',
+    //  justifyContent: 'flex-end',
+      height:20
     }
 })
